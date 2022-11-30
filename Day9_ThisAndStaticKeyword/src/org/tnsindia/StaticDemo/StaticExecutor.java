cb4683240fe130110e53492a827b46b0ff5a7d1f
Admin <Admin@DESKTOP-UR8EQ74>
2022-11-30 19:18:16 +0530

    first commit

---
+++ Day9_ThisAndStaticKeyword/src/org/tnsindia/StaticDemo/StaticExecutor.java
@@ -0,0 +1,13 @@
+package org.tnsindia.StaticDemo;
+//Driver class
+public class StaticExecutor {
+
+	public static void main(String[] args) {
+		/*StaticKeywordWithVariable s=new StaticKeywordWithVariable();
+		System.out.println(s.a);*/
+		System.out.println(Staticdemo.a);
+		Staticdemo.accept();
+	
+	}
+
+}
