b529741870a09e5a082b34f04f907cad453034d8
Admin <Admin@DESKTOP-UR8EQ74>
2022-12-12 14:56:53 +0530

    first commit

---
+++ Day13_ExceptionHandling/src/org/tnsindia/exceptiondemo/ExecutionForThrow.java
@@ -0,0 +1,12 @@
+package org.tnsindia.exceptiondemo;
+
+//driver class
+public class ExecutionForThrow {
+
+	public static void main(String[] args) {
+		DemoonThrowKeyword d=new DemoonThrowKeyword();
+		d.display("Welcome to TNS India");
+
+	}
+
+}
