6c85cf62f6e36aaaa0f2278f37f2dfed679e95d8
Admin <Admin@DESKTOP-UR8EQ74>
2022-12-15 20:10:15 +0530

    first commit

---
+++ Day17_JavaAnnotations/src/org/tnsindia/builtinannotationdemo/Parent.java
@@ -0,0 +1,15 @@
+package org.tnsindia.builtinannotationdemo;
+
+//Super class
+public class Parent {
+	
+	public String name="Nita R";
+	public void display()
+	{
+		System.out.println("Parent Name: "+name);
+	}
+	
+	
+	
+
+}
